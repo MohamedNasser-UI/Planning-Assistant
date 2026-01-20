26310ea1a6e16d21a70a904ba7b6695912620a1d
MohamedNasser-UI <acc.mohamed.abdelnasser@gmail.com>
2026-01-20 15:24:38 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  base: "/Planning-Assistant/",
   plugins: [react()],
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
 });
-
